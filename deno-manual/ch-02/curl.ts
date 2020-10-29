227f45ab9415369badb03cfe1a5931f191ed2297
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-10-29 15:22:36 -0500

    getting started

---
+++ deno-manual/ch-02/curl.ts
@@ -0,0 +1,8 @@
+const url = Deno.args[0];
+const res = await fetch(url);
+
+const body = new Uint8Array(await res.arrayBuffer());
+await Deno.stdout.write(body);
+
+
+// deno run --allow-net curl.ts https://example.com
